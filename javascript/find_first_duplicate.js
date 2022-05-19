2ceec02fd17849d1bffc167fc5eb79aff8fb7b81
Granit Zymeri <gzymeri55@gmail.com>
2022-05-19 16:32:42 -0400

    first commit

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,3 +1,4 @@
+
 function findFirstDuplicate(arr) {
   // type your code here
 }
@@ -17,3 +18,33 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+
+
+// create a function with an array of numbers as argument
+  function findIDuplicate(arr){
+
+  //declare an empty array to store the results
+      let results = []
+
+  // itereate through the numbers array using a loop
+    for (num of arr) {
+      console.log(num)
+  
+     //using if/else statement, check to see if there's a number more than once
+      //if yes, return the first number found to the new array
+      if (results[nums]) {
+      return num
+    }
+     //else return -1
+     else {
+       return results[num] = num
+     }
+  }
+   //return -1
+  return -1
+}
+
+
+
